6dacec7c86ac8d3cbb135935814560e4de79f7e6
Flora Calbris <flora.calbris@gmail.com>
2018-09-14 15:43:58 +0200

    maj

--- src/main/java/dev/top/StartupDataInit.java
+++ src/main/java/dev/top/StartupDataInit.java
@@ -1,27 +1,32 @@
 package dev.top;
 
-import dev.top.entities.Version;
-import dev.top.repos.VersionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import dev.top.entities.Version;
+import dev.top.repos.VersionRepo;
+
 @Component
 public class StartupDataInit {
 
-    @Autowired
-    VersionRepo versionRepo;
+	// bonne pratique : plutot utiliser le constructeur et non faire l'injection
+	// sur le champ qui est privé (compliqué pour les tests unitaires)
+	@Autowired
+	private VersionRepo versionRepo;
 
-    @EventListener(ContextRefreshedEvent.class)
-    public void init() {
+	// on insere un jeu de données a chaque fois que l'appli est lancée par
+	// init()
+	@EventListener(ContextRefreshedEvent.class)
+	public void init() {
 
-        if(this.versionRepo.count() <= 0) {
-            this.versionRepo.save(new Version("v1"));
-            this.versionRepo.save(new Version("v2"));
-            this.versionRepo.save(new Version("v3"));
-            this.versionRepo.save(new Version("v4"));
-        }
+		if (this.versionRepo.count() <= 0) {
+			this.versionRepo.save(new Version("v1"));
+			this.versionRepo.save(new Version("v2"));
+			this.versionRepo.save(new Version("v3"));
+			this.versionRepo.save(new Version("v4"));
+		}
 
-    }
+	}
 }
